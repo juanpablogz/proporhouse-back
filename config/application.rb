b8a3f2e348ab09ec387a246e45ec8b81f904901c
juan pablo gomez zapata <juanpablogomezzapata@gmail.com>
2021-04-05 18:46:00 -0500

    test

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,7 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
+    #hello world
     # ActiveAdmin needs the following middlewares to work properly.
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
